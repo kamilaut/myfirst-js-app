e7ae18d1b23e3a5d2b3a85d2d78dce55b95038e9
kamilaut <kamila.utyukova@gmail.com>
2022-07-04 21:49:39 +0200

    replaced Pokemonlist
    replaced primitive values with complex datatypes: objects and arrays. Stored 1- name (string)
    2- height (number)
    3- types (array of strings)

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
-let pokemonList = {'Nidorino, Wigglytuff, Tentacruel, Chansey'};
-let Nidorino, Wigglytuff, Tentacruel, Chansey = {
-height = 10,
-type = 'psychicfairy',
-abilities = 'Soundproof, Technician, Filter'
-};
+let pokemonList = [
+  { name: "Nidorino", height: 0.9, type: 'poison' , skills: ['Poison-point, Hustle, Rivalry']},
+  { name: "Wigglytuff", height: 1.0, type: 'fairy' , skills: ['Cute-charm', 'Frisk']},
+  { name: "Chansey", height: 1.1, type: 'normal' , skills: ['Natural-cure', 'Serene-grace', 'Healer']}
+];
 document.write(pokemonList);
