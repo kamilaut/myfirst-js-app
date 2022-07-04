f8dd21eb7364717b908c537e6c85b71203fb2a86
kamilaut <kamila.utyukova@gmail.com>
2022-07-04 18:53:35 +0200

    fixing linking css file
    correcting the way rel attribute is used in the link tag

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-let favoriteFood = 'IceCream';
-document.write(favoriteFood);
- 
+let pokemonList = {'Nidorino, Wigglytuff, Tentacruel, Chansey'};
+let Nidorino, Wigglytuff, Tentacruel, Chansey = {
+height = 10,
+type = 'psychicfairy',
+abilities = 'Soundproof, Technician, Filter'
+};
+document.write(pokemonList);
