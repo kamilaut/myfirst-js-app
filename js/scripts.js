c156f02a6710d891bb92c6cd43430189ccd6dcf6
kamilaut <kamila.utyukova@gmail.com>
2022-07-22 21:52:11 +0200

    exercise 1.3 (create a for loop to iterate over each object within that array).

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,12 @@ let pokemonList = [
   { name: "Wigglytuff", height: 1.0, type: 'fairy' , skills: ['Cute-charm', 'Frisk']},
   { name: "Chansey", height: 1.1, type: 'normal' , skills: ['Natural-cure', 'Serene-grace', 'Healer']}
 ];
-document.write(pokemonList);
+
+for (let i=0; i<pokemonList.length; i++) {
+    //writes special text for pokemons with a height over 1
+    if (pokemonList[i].height>1) {
+        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s big!')
+    } else {
+        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')</p>')
+    }
+}
