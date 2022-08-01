a7219d93f378c659c1c7270346e3185853dd960c
kamilaut <kamila.utyukova@gmail.com>
2022-08-01 14:46:38 +0200

    forEach() Loop implemented

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,14 @@ let pokemonList = [
   { name: "Chansey", height: 1.1, type: 'normal' , skills: ['Natural-cure', 'Serene-grace', 'Healer']}
 ];
 
-for (let i = 0; i<pokemonList.length; i++) {
+let pokemonList = [
+  { name: "Nidorino", height: 0.9, type: 'poison' , skills: ['Poison-point, Hustle, Rivalry']},
+  { name: "Wigglytuff", height: 1.0, type: 'fairy' , skills: ['Cute-charm', 'Frisk']},
+  { name: "Chansey", height: 1.1, type: 'normal' , skills: ['Natural-cure', 'Serene-grace', 'Healer']}
+];
 
-  document.write("<p>" + pokemonList[i].name + ' (height:' + pokemonList[i].height +  ')');
 
-  //writes special text for pokemons with a height over 1
-  if (pokemonList[i].height>1) {
-    document.write(" - Wow, that\'s big!</p>")
-  }
-
-  document.write("</p>")
+function myLoopFunction(pokemon) {
+  console.log(pokemon.name + ' is ' + pokemon.height + pokemon.type );
 }
+pokemonList.forEach(myLoopFunction);
