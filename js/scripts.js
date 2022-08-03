4bd5a9fb0bb8ad3ba6c277dd4d977b76ad84f1fd
kamilaut <kamila.utyukova@gmail.com>
2022-08-03 18:17:34 +0200

    forEach() Loops and IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,34 @@
-let pokemonList = [
-  { name: "Nidorino", height: 0.9, type: 'poison' , skills: ['Poison-point, Hustle, Rivalry']},
-  { name: "Wigglytuff", height: 1.0, type: 'fairy' , skills: ['Cute-charm', 'Frisk']},
-  { name: "Chansey", height: 1.1, type: 'normal' , skills: ['Natural-cure', 'Serene-grace', 'Healer']}
-];
+let pokemonRepository = (function () {
 
-let pokemonList = [
-  { name: "Nidorino", height: 0.9, type: 'poison' , skills: ['Poison-point, Hustle, Rivalry']},
-  { name: "Wigglytuff", height: 1.0, type: 'fairy' , skills: ['Cute-charm', 'Frisk']},
-  { name: "Chansey", height: 1.1, type: 'normal' , skills: ['Natural-cure', 'Serene-grace', 'Healer']}
-];
+  let pokemonList = [
+    { name: "Nidorino", height: 0.9,
+    type: 'poison' ,
+    skills: ['Poison-point, Hustle, Rivalry']},
+
+    { name: "Wigglytuff",
+    height: 1.0, type: 'fairy' ,
+    skills: ['Cute-charm', 'Frisk']},
+
+    { name: "Chansey",
+    height: 1.1,
+    type: 'normal' ,
+    skills: ['Natural-cure', 'Serene-grace', 'Healer']}
+  ];
 
 
-function myLoopFunction(pokemon) {
-  console.log(pokemon.name + ' is ' + pokemon.height + pokemon.type );
-}
-pokemonList.forEach(myLoopFunction);
+  function getAll () {
+    return pokemonList;
+  }
+  function add (pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  return {
+    getAll: getAll,
+    add: add
+  }
+})();
+
+
+console.log(pokemonRepository.getAll());//[]
+pokemonRepository.add({name: "Pikachu"});
